070fff0ad6659b0d804431574e1c3d05fe9892da
Theo PAYEN <tt.payen@gmail.com>
2022-05-15 22:29:15 +0200

    Update creat_group_teams.ps1

--- creat_group_teams.ps1
+++ creat_group_teams.ps1
@@ -1,1 +1,33 @@
+# prérequis
+function prerequis {
+    # installe les modules requis pour la bon fonctionnement du script
+    Install-Module MicrosoftTeams -Force
+    Install-Module MSOnline -Force
+    
+    # install Azure AD
+    Install-Module AzureAD -Force
+    Import-Module AzureAD -Force
+}
 
+function connection {
+    # permet de se connecter à microsoft
+    Connect-MicrosoftTeams 
+    Connect-AzureAD
+}
+function main {
+    # appel les fonction
+    prerequis
+    connection
+    # récupère toute la liste des utilisateurs dans Azure AD dans un for
+    Get-AzureADUser | ForEach-Object {
+        # récupère le departement de l'utilisateur
+        $Department = $($_.Department)
+        # récupère l'equipe teams avec le meme nom de departement
+        if (Get-team -DisplayName $Department -eq $null){
+            write-host "crée l'equipe $Department"
+            new-Team -DisplayName $Department
+        }
+    }   
+}
+
+main
